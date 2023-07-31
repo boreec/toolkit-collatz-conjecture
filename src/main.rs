96f3270003d0d59690e469a45d6487ba8589f762
Cyprien Borée <51238594+boreec@users.noreply.github.com>
2023-08-01 00:22:30 +0900

    feature: add -n argument to generate a sequence for any number (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Default, Parser)]
+struct Args {
+    #[clap(short)]
+    n: Option<u128>   
 }
+
+fn collatz_sequence(n: u128) -> u128 {
+    if n == 1 {
+        println!("{}", n);
+        return 1;
+    }
+    print!("{},", n);
+    if n % 2 == 0 {
+        return collatz_sequence(n / 2);
+    }
+    return collatz_sequence(3 * n + 1);
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if let Some(n) = args.n {
+        collatz_sequence(n);
+    }
+}
+
