9d65ce619b18e4f664691d34fb53f4497293d3ba
Cyprien Borée <cyprien.boree@tuta.io>
2023-08-02 00:23:51 +0900

    feat: add CollatzSequence struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+mod core;
+
 use clap::Parser;
+use crate::core::CollatzSequence;
+
 
 #[derive(Debug, Default, Parser)]
 struct Args {
@@ -6,23 +10,12 @@ struct Args {
     n: Option<u128>   
 }
 
-fn collatz_sequence(n: u128) -> u128 {
-    if n == 1 {
-        println!("{}", n);
-        return 1;
-    }
-    print!("{},", n);
-    if n % 2 == 0 {
-        return collatz_sequence(n / 2);
-    }
-    return collatz_sequence(3 * n + 1);
-}
-
 fn main() {
     let args = Args::parse();
 
     if let Some(n) = args.n {
-        collatz_sequence(n);
+        let collatz_sequence = CollatzSequence::new(n);
+        println!("{:?}", collatz_sequence);
     }
 }
 
