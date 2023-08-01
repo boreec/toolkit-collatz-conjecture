2f5af05180cf26af127dda16bd705d3098f0b6b5
Cyprien Borée <51238594+boreec@users.noreply.github.com>
2023-08-02 00:24:56 +0900

    feat: add CollatzSequence struct (#2)

---
+++ src/core.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+pub struct CollatzSequence {
+    hailstone: Vec<u128>,
+    starting_number: u128
+}
+
+impl CollatzSequence {
+    pub fn new(starting_number: u128) -> Self {
+        let mut collatz_sequence = CollatzSequence {
+            hailstone: Vec::new(),
+            starting_number,
+        };
+        collatz_sequence.compute();
+        collatz_sequence
+    }
+
+    pub fn compute(&mut self) {
+        if self.starting_number == 0 {
+            return;
+        }
+
+        self.hailstone.push(self.starting_number);
+ 
+        let mut i = self.starting_number;
+        while i != 1 {
+            i = if i % 2 == 0 { i / 2 } else { 3 * i + 1 };
+            self.hailstone.push(i);
+        }
+    }
+}
+
