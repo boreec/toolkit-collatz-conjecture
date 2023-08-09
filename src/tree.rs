06ea66930c9abca387f932f1bb3bb5b3276eea36
Cyprien Borée <cyprien.boree@tuta.io>
2023-08-09 21:55:26 +0900

    feat: add --tree and algorithm
    The `--tree` argument can be used to build the bottom-up tree from 1 to
    `n`. This behavior is asserted by a simple unit test which verify that
    the tree from 1 to 10 contains the correct values.

--- src/tree.rs
+++ src/tree.rs
@@ -1,24 +1,47 @@
-use petgraph::graph::DiGraph;
-
 pub struct CollatzTree {
-    tree: DiGraph<u128, ()>,
-    target: u128,
+    pub adjacency_matrix: Vec<(u128, Option<u128>)>,
+    pub target: u128,
 }
 
 impl CollatzTree {
     pub fn new(n: u128) -> Self {
-        return CollatzTree {
-            tree: CollatzTree::build_tree(n),
+        let mut tree = CollatzTree {
+            adjacency_matrix: vec![],
             target: n,
         };
+        tree.build();
+        tree
     }
 
-    fn build_tree(n: u128) -> DiGraph<u128, ()> {
-        let mut root = DiGraph::<u128, ()>::new();
+    fn build(&mut self) {
+        for i in 1..=self.target {
+            self.adjacency_matrix.push((
+                i * 2,                                             // even case
+                if i % 6 == 4 { Some((i - 1) / 3) } else { None }, // odd case
+            ))
+        }
+    }
+}
 
-        root.add_node(1);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        // to do
-        root
+    #[test]
+    fn test_collatz_tree_build() {
+        let expected = vec![
+            (2, None),
+            (4, None),
+            (6, None),
+            (8, Some(1)),
+            (10, None),
+            (12, None),
+            (14, None),
+            (16, None),
+            (18, None),
+            (20, Some(3)),
+        ];
+        let built = CollatzTree::new(10).adjacency_matrix;
+        assert_eq!(expected, built)
     }
 }
