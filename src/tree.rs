384b4070855de43f357c74ed2df0677e5b9080cf
Cyprien Borée <cyprien.boree@tuta.io>
2023-08-12 17:53:33 +0900

    chore: remove dead parentless trees from --tree

--- src/tree.rs
+++ src/tree.rs
@@ -1,27 +1,42 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 
 pub struct CollatzTree {
-    pub adjacency_matrix: Vec<(u128, Option<u128>)>,
+    pub tree: HashMap<u128, (Option<u128>, Option<u128>)>,
     pub target: u128,
 }
 
 impl CollatzTree {
     pub fn new(n: u128) -> Self {
-        let mut tree = CollatzTree {
-            adjacency_matrix: vec![],
+        let mut t = CollatzTree {
+            tree: HashMap::new(),
             target: n,
         };
-        tree.build();
-        tree
+        t.build();
+        t
     }
 
     fn build(&mut self) {
-        for i in 1..=self.target {
-            self.adjacency_matrix.push((
-                i * 2,                                             // even case
-                if i % 6 == 4 { Some((i - 1) / 3) } else { None }, // odd case
-            ))
+        self.tree.insert(1, (Some(2), None));
+        let mut nodes_to_explore = vec![2];
+        while let Some(node) = nodes_to_explore.pop() {
+            if self.tree.contains_key(&node) {
+                continue;
+            }
+            let p_1 = if node * 2 <= self.target {
+                nodes_to_explore.push(node * 2);
+                Some(node * 2)
+            } else {
+                None
+            };
+            let p_2 = if node % 6 == 4 {
+                nodes_to_explore.push((node - 1) / 3);
+                Some((node - 1) / 3)
+            } else {
+                None
+            };
+            self.tree.insert(node, (p_1, p_2));
         }
     }
 
@@ -32,10 +47,12 @@ impl CollatzTree {
         writeln!(&mut file, "digraph to_{} {{", self.target)?;
         writeln!(&mut file, "\trankdir=BT;")?;
         self.define_node_colors(&mut file)?;
-        for (i, branches) in self.adjacency_matrix.iter().enumerate() {
-            writeln!(&mut file, "\t{} -> {};", i + 1, branches.0)?;
-            if let Some(odd_number) = branches.1 {
-                writeln!(&mut file, "\t{} -> {};", i + 1, odd_number)?;
+        for (key, value) in self.tree.iter() {
+            if let Some(p_1) = value.0 {
+                writeln!(&mut file, "\t{} -> {:?};", key, p_1)?;
+            }
+            if let Some(p_2) = value.1 {
+                writeln!(&mut file, "\t{} -> {:?};", key, p_2)?;
             }
         }
         writeln!(&mut file, "}}")?;
@@ -47,22 +64,15 @@ impl CollatzTree {
         &self,
         file: &mut File,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        for i in 0..self.adjacency_matrix.len() {
-            if (i + 1) % 2 == 0 {
-                writeln!(file, "{} [color=green, style=filled];", i + 1)?;
-            } else {
-                writeln!(file, "{} [color=gold, style=filled];", i + 1)?;
-            }
-        }
+        let mut sorted_keys: Vec<_> = self.tree.keys().copied().collect();
+        sorted_keys.sort();
 
-        for node in self.adjacency_matrix.iter() {
-            if node.0 > self.adjacency_matrix.len() as u128 {
-                writeln!(file, "{} [color=green, style=filled];", node.0)?;
-            }
-            if let Some(odd) = node.1 {
-                if odd > self.adjacency_matrix.len() as u128 {
-                    writeln!(file, "{} [color=gold, style=filled];", odd)?;
-                }
+        writeln!(file, "\t1 [color=gold, style=filled];")?;
+        for key in sorted_keys.iter().skip(1) {
+            if key % 2 == 0 {
+                writeln!(file, "\t{} [color=green, style=filled];", key)?;
+            } else {
+                writeln!(file, "\t{} [color=gold, style=filled];", key)?;
             }
         }
         Ok(())
